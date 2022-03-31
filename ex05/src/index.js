c941c8d1a04cbf37527bd209989ef77891a520cf
Dedo-unuka <dzanazovko@gmail.com>
2022-03-31 10:17:05 +0200

    Adding ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // only change code bellow this line
+         var numOne;
+         var numTwo;
+         var product = numOne * numTwo;
+    // only change code above this line
+    return product;
+}
+
+console.log(main(2.5, 2.0)); 
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));       // change this line
+module.exports = main;
