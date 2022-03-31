b24c5a83226751e3a71b791cdcd3ef21a7d59fba
Dedo-unuka <dzanazovko@gmail.com>
2022-03-31 09:42:46 +0200

    Adding ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -13,7 +13,8 @@ function main(numOne, numTwo) {
     return quotient;
 }
 
-console.log(main(15, 3));
+console.log(main(15, 3));  // change this line
 console.log(main(25, 5));
 console.log(main(420, 10));
 console.log(main(55, 0));
+module.exports = main;
