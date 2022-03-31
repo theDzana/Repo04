e592b4f3010a8273dbb2007fd05a16c65259a97b
Dedo-unuka <dzanazovko@gmail.com>
2022-03-31 10:22:01 +0200

    Adding ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // only change code bellow this line
+         var numOne;
+         var numTwo;
+         var myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0)); 
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));       // change this line
+module.exports = main;
