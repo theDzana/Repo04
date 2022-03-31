b0d92a67571a4afebe286697b88f8d7b26a11295
Dedo-unuka <dzanazovko@gmail.com>
2022-03-31 09:58:25 +0200

    Adding ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // only change code bellow this line
+         var myDecimal = 5.7;
+         var myDecimalE = 2.54e-3;
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
